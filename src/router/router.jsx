72ab0594de7c10e561974c7bab1285fd049794fc
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-12-31 12:09:14 +0600

    update: completed the router setup

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,8 +2,11 @@ import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 
-const Home = lazy(() => import('../pages/Home/Home'));
-const AllProducts = lazy(() => import('../pages/AllProducts/AllProducts'));
+const Home = lazy(() => import('../pages/Home'));
+const AllProducts = lazy(() => import('../pages/AllProducts'));
+const Cart = lazy(() => import('../pages/Cart'));
+const Login = lazy(() => import('../pages/auth/Login'));
+const Register = lazy(() => import('../pages/auth/Register'));
 
 const router = createBrowserRouter([
     {
@@ -15,8 +18,20 @@ const router = createBrowserRouter([
                 element: <Home />,
             },
             {
-                path: '/allProducts',
+                path: '/all-products',
                 element: <AllProducts />,
+            },
+            {
+                path: '/cart',
+                element: <Cart />,
+            },
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/register',
+                element: <Register />,
             },
         ],
     },
