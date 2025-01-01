0f6bc0e705914a1a9a2b4ea0845375d358ef1abc
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-01-01 07:00:24 +0600

    update: added registration process

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,13 +1,53 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 import { FaUserPlus } from 'react-icons/fa';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import notify from '../../utilities/notify';
 import AuthLayout from '../../components/Auth/AuthLayout';
 import FormInput from '../../components/Auth/FormInput';
 
 export default function Register() {
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const navigate = useNavigate();
+    const axiosSecure = useAxiosSecure();
+    const { mutate, isPending, isSuccess, isError, error } = useMutation({
+        mutationFn: async () =>
+            axiosSecure.post('/register', {
+                name: data.name,
+                email: data.email,
+                password: data.password,
+            }),
+    });
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Handle registration logic here
+
+        if (!data.name || !data.email || !data.password) {
+            return notify('error', 'All fields are required');
+        } else if (data.password.length < 6) {
+            return notify('error', 'Password must be at least 6 characters');
+        } else if (data.password !== data.confirmPassword) {
+            return notify('error', 'Passwords do not match');
+        }
+
+        mutate();
     };
+
+    useEffect(() => {
+        if (isSuccess) {
+            notify('success', 'Registration successful');
+            navigate('/login');
+        } else if (isError) {
+            notify('error', error.response.data || error.message);
+        }
+    }, [isSuccess, isError, error, navigate]);
 
     return (
         <AuthLayout
@@ -21,34 +61,48 @@ export default function Register() {
                         type='text'
                         id='fullName'
                         placeholder='Enter your full name'
+                        onChange={(e) =>
+                            setData({ ...data, name: e.target.value })
+                        }
                     />
                     <FormInput
                         label='Email address'
                         type='email'
                         id='email'
                         placeholder='Enter your email'
+                        onChange={(e) =>
+                            setData({ ...data, email: e.target.value })
+                        }
                     />
                     <FormInput
                         label='Password'
                         type='password'
                         id='password'
                         placeholder='Create a password'
+                        onChange={(e) =>
+                            setData({ ...data, password: e.target.value })
+                        }
                     />
                     <FormInput
                         label='Confirm Password'
                         type='password'
                         id='confirmPassword'
                         placeholder='Confirm your password'
+                        onChange={(e) =>
+                            setData({
+                                ...data,
+                                confirmPassword: e.target.value,
+                            })
+                        }
                     />
                 </div>
 
                 <button
                     type='submit'
-                    className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
+                    className='group relative w-full flex items-center gap-x-2 justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
+                    disabled={isPending}
                 >
-                    <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
-                        <FaUserPlus className='h-5 w-5 text-blue-500 group-hover:text-blue-400' />
-                    </span>
+                    <FaUserPlus className='h-5 w-5 text-white' />
                     Create Account
                 </button>
 
