7cc855960bfe68f4a421aa844c21118c09490c3e
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-01-01 07:25:20 +0600

    update: made the Navbar component working

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,9 +1,29 @@
+import { useContext } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { BsBoxSeam } from 'react-icons/bs';
 import { GoHome } from 'react-icons/go';
 import { RiShoppingCartLine, RiLoginCircleLine } from 'react-icons/ri';
+import UserContext from '../contexts/UserContext';
+import AuthContext from '../contexts/AuthContext';
+import CartContext from '../contexts/CartContext';
 
 export default function Navbar() {
+    const { user, setUser } = useContext(UserContext);
+    const { setAuthToken } = useContext(AuthContext);
+    const { cart } = useContext(CartContext);
+
+    const handleLogout = () => {
+        localStorage.removeItem('auth');
+
+        setUser({ data: null });
+        setAuthToken({ token: null });
+    };
+
+    const totalQuantity = cart?.reduce(
+        (total, curr) => total + curr.quantity,
+        0
+    );
+
     return (
         <nav className='sticky top-0 bg-white shadow-sm px-5 py-7 flex items-center justify-end z-50'>
             {/* Logo */}
@@ -54,25 +74,38 @@ export default function Navbar() {
                     >
                         <div className='relative'>
                             <RiShoppingCartLine className='size-5' />
-                            <span className='absolute -top-2 -right-2 bg-blue-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center'>
-                                8
-                            </span>
+                            {totalQuantity && user?.data?.id ? (
+                                <span className='absolute -top-2 -right-2 bg-blue-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center'>
+                                    <span className='absolute -top-2 -right-2 bg-blue-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center'>
+                                        {totalQuantity}
+                                    </span>
+                                </span>
+                            ) : null}
                         </div>
                         Cart
                     </NavLink>
                 </div>
 
-                <NavLink
-                    to='/login'
-                    className={({ isActive }) =>
-                        `flex items-center gap-2 hover:text-blue-700 transition-colors ${
-                            isActive ? 'text-[#3B82F6]' : 'text-gray-600'
-                        }`
-                    }
-                >
-                    <RiLoginCircleLine className='size-5' />
-                    <span>Login</span>
-                </NavLink>
+                {user?.data?.id ? (
+                    <button
+                        className='flex items-center gap-2 text-gray-600 hover:text-blue-300 transition-colors'
+                        onClick={handleLogout}
+                    >
+                        <RiLoginCircleLine className='w-5 h-5' /> Logout
+                    </button>
+                ) : (
+                    <NavLink
+                        to='/login'
+                        className={({ isActive }) =>
+                            `flex items-center gap-2 hover:text-blue-700 transition-colors ${
+                                isActive ? 'text-[#3B82F6]' : 'text-gray-600'
+                            }`
+                        }
+                    >
+                        <RiLoginCircleLine className='size-5' />
+                        <span>Login</span>
+                    </NavLink>
+                )}
             </div>
         </nav>
     );
