2159c3fd9989447e16b527f8d60b995f42b432f3
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-01-01 07:14:56 +0600

    update: configured with context provider wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthContextProvider } from './contexts/AuthContext.jsx';
+import { UserContextProvider } from './contexts/UserContext.jsx';
+import { CartContextProvider } from './contexts/CartContext.jsx';
 import router from './router/router.jsx';
 import './index.css';
 
@@ -10,7 +13,13 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
+            <AuthContextProvider>
+                <UserContextProvider>
+                    <CartContextProvider>
+                        <RouterProvider router={router} />
+                    </CartContextProvider>
+                </UserContextProvider>
+            </AuthContextProvider>
         </QueryClientProvider>
     </StrictMode>
 );
